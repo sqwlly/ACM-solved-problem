3fa1094a20c562e440d7978ae3f9ae8217650c0f
sqwlly <sqw.lucky@gmail.com>
2018-09-06 00:06:08 +0800

    新增B题代码

---
+++ ACM-ICPC/南京赛区网络预赛_2018/B.cc
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+const int N = 1e5+10;
+int loc[N][101],up[101],n,m,k,x,y,T,w;
+
+int main()
+{
+	scanf("%d",&T);
+	while(T--)
+	{
+		scanf("%d%d%d",&n,&m,&k);
+		memset(loc,0,sizeof loc);
+		memset(up,0,sizeof up);
+		for(int i = 0; i < k; ++i){
+			scanf("%d%d",&x,&y);
+			loc[x][y] = 1;
+		}
+		long long ans = 0;
+		for(int i = 1; i <= n; ++i){
+			for(int j = 1; j <= m; ++j){
+				if(loc[i][j])
+					up[j] = i;
+			}
+			for(int j = 1; j <= m; ++j){ //以(i,j)为右下角的矩阵的子矩阵个数
+				int minH = 1<<30; //铜奖矩阵长为K时高最大为多少
+				for(int K = j; K >= 1; --K){ //枚举矩阵长度
+					//K代表以(i,j)为右下角，长为K的矩阵左边界下标
+					minH = std::min(minH,i-up[K]); //i-up[K]代表此列能取得的最大高
+					ans += minH;
+				}
+			}
+		}
+		printf("Case #%d: %lld\n",++w,ans);
+	}
+	return 0;
+}
