2a4e8c35298bfa6381b6f615a7f0df833e0df464
sqwlly <sqw.lucky@gmail.com>
2018-09-07 13:49:09 +0800

    update

--- ACM-ICPC/南京赛区网络预赛_2018/B.cc
+++ ACM-ICPC/南京赛区网络预赛_2018/B.cc
@@ -17,11 +17,11 @@ int main()
 		long long ans = 0;
 		for(int i = 1; i <= n; ++i){
 			for(int j = 1; j <= m; ++j){
-				if(loc[i][j])
-					up[j] = i; //第j列中为黑块的行(在1~i行中取最大行标)
+				if(loc[i][j]) //第j列中为黑块的行(在1~i行中取最大行标)
+					up[j] = i;
 			}
 			for(int j = 1; j <= m; ++j){ //以(i,j)为右下角的矩阵的子矩阵个数
-				int minH = 1<<30; //铜奖矩阵长为K时高最大为多少
+				int minH = 1<<30; //统计矩阵长为K时高最大为多少
 				for(int K = j; K >= 1; --K){ //枚举矩阵长度
 					//K代表以(i,j)为右下角，长为K的矩阵左边界下标
 					minH = std::min(minH,i-up[K]); //i-up[K]代表此列能取得的最大高
