d540b4f30c3d4e54fa97afd1a401b6fa49d92993
sqwlly <sqw.lucky@gmail.com>
2018-11-25 15:47:10 +0800

    update codeforces

--- Codeforces/Codeforces Round #524 (Div. 2)/C.cc
+++ Codeforces/Codeforces Round #524 (Div. 2)/C.cc
@@ -1,7 +1,7 @@
 /*************************************************************************
     > File Name: C.cc
     > Author: sqwlly
-    > Mail: sqw.lucky@gmail.com 
+    > Mail: sqw.lucky@gmail.com
     > Created Time: 2018年11月24日 星期六 16时20分44秒
  ************************************************************************/
 
@@ -10,80 +10,60 @@ typedef long long LL;
 using namespace std;
 
 struct rec{
-	int x1,y1,x2,y2;
-	rec() {}
-	rec(int a,int b,int c,int d) { x1 = a; y1 = b; x2 = c; y2 = d;}
+    int x1,y1,x2,y2;
+    rec() {}
+    rec(int a,int b,int c,int d) { x1 = a; y1 = b; x2 = c; y2 = d;}
 };
 int n,m;
-bool comb(rec a,rec b)
+bool comb(rec t)
 {
-	if(a.x1 == b.x1 && a.y1 == b.y1) return 1;
-	if(abs((a.x1+a.x2)/2-(b.x1+b.x2)/2)<((a.x2+b.x2-a.x1-b.x1)/2) && abs((a.y1+a.y2)/2-(b.y1+b.y2)/2)<((a.y2+b.y2-a.y1-b.y1)/2))
-	return true;
-	return false;
-}
-
-void color(rec a,LL &sum_b,LL &sum_w,int d)
-{
-	LL a_m = a.x2 - a.x1, a_n = a.y2 - a.y1;
-	if(a_m <= 0 || a_n <= 0) return;
-	if(a.x1 % 2 == 1 && a.y1 % 2 == 1) {
-		sum_b -= (a_n * a_m / 2) * d;
-		sum_w += (a_n * a_m / 2) * d;
-	}else{
-		sum_b -= (a_n * a_m / 2 + 1) * d;
-		sum_w += (a_n * a_m / 2 + 1) * d;
-	}
-}
-
-int main()
-{
-#ifndef ONLINE_JUDGE
-    freopen("input.in","r",stdin);
-#endif
-	int T;
-	cin>>T;
-	while(T--) {
-		rec a,b;
-		cin>>n>>m;
-		cin>>a.x1>>a.y1>>a.x2>>a.y2;
-		cin>>b.x1>>b.y1>>b.x2>>b.y2;
-		LL sum_w = 0, sum_b = 0;
-		if(n & 1) {
-			if(m & 1) {
-				sum_w = 1LL * n * m / 2 + 1;
-				sum_b = 1LL * n * m / 2;
-			}else{
-				sum_w = 1LL * n * m / 2;
-				sum_b = 1LL * n * m / 2;
-			}
-		}else{
-			if(m & 1) {
-				sum_w = 1LL * n * m / 2;
-				sum_b = 1LL * n * m / 2;
-			}else{
-				sum_w = 1LL * n * m / 2;
-				sum_b = 1LL * n * m / 2;
-			}
-		}
-		if(comb(a,b) == 0) {
-			color(a,sum_b,sum_w,1);
-			color(b,sum_b,sum_w,-1);
-		}else{
-			rec t1,t2;
-			if(a.x1 < b.x1 && a.y1 < b.y1) {
-				t1 = rec{a.x1,a.y1,b.x1,a.y2};
-				t2 = rec{b.x1,a.y1,a.x2,b.y1};
-			}else{
-				t1 = rec{a.x1,a.y1,b.x1,a.y2};
-				t2 = rec{b.x1,b.y2,a.x2,a.y2};
-			}
-			color(t1,sum_b,sum_w,1);
-			color(t2,sum_b,sum_w,1);
-			color(b,sum_b,sum_w,-1);
-			
-		}
-		cout << sum_w <<' '<< sum_b << endl;
-	}
+    if(t.x1 <= t.x2 && t.y1 <= t.y2)
+        return 1;
     return 0;
 }
+
+LL all(rec a)
+{
+    LL n = a.y2 - a.y1 + 1;
+    LL m = a.x2 - a.x1 + 1;
+    return n * m;
+}
+
+LL color_w(rec a)
+{
+    LL a_m = a.x2 - a.x1 + 1, a_n = a.y2 - a.y1 + 1;
+    if((a.x1 + a.y1) % 2 == 0) {
+        return ((a_m * a_n + 1)/ 2);
+    }
+    return  (a_m * a_n / 2);
+}
+
+LL color_b(rec a)
+{
+    return all(a) - color_w(a);
+}
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.in", "r", stdin);
+#endif
+    int T;
+    cin >> T;
+    while (T--) {
+        rec a, b;
+        cin >> n >> m;
+        cin >> a.x1 >> a.y1 >> a.x2 >> a.y2;
+        cin >> b.x1 >> b.y1 >> b.x2 >> b.y2;
+        LL sum_w = (1LL * n * m + 1) / 2;
+        sum_w += color_b(a);
+        sum_w -= color_w(b);
+        rec t = rec{max(a.x1, b.x1), max(a.y1, b.y1), min(a.x2, b.x2), min(a.y2, b.y2)};
+        if (comb(t) == 1) {
+            sum_w -= color_b(t);
+        }
+        cout << sum_w << ' ' << 1LL * n * m - sum_w << endl;
+    }
+    return 0;
+}
+
