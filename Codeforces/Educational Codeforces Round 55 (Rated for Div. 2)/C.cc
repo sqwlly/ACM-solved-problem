6b094c8d05fc72e350f524814e80af914466dd64
sqwlly <sqw.lucky@gmail.com>
2018-12-01 17:29:18 +0800

    update poj

--- Codeforces/Educational Codeforces Round 55 (Rated for Div. 2)/C.cc
+++ Codeforces/Educational Codeforces Round 55 (Rated for Div. 2)/C.cc
@@ -1,94 +1,33 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
-#include <math.h>
-#include <map>
-#include <set>
+#include <bits/stdc++.h>
+
 using namespace std;
-
-#define mp make_pair
-#define pii pair<int, int>
-#define s second
-#define f first
-#define ld long double
-#define pb push_back
-#define ll long long
-
-bool comparer(pii a, pii b)
-{
-    if(a.f < b.f)
-        return true;
-    else if (a.f > b.f)
-        return false;
-
-    return a.s > b.s;
-}
-
-bool comparer2(vector<int> a, vector<int> b)
-{
-    //cout << a.size() << " " << b.size() << endl;
-    return b.size() < a.size();
-}
-
+vector < long long> v[100100];
+long long a[100100];
 int main()
 {
-    ios_base::sync_with_stdio(0); cout.tie(0); cin.tie(0); cerr.tie(0);
-
-
-    int n, m;
-    cin >> n >> m;
-    vector<pii > people;
-    vector<vector<int> > a;
-
-    for(int i = 0; i < n; i++)
-    {
-        pii x;
-        cin >> x.f >> x.s;
-        people.pb(x);
+    long long n,m,x,y,i;  cin>>n>>m;
+    for(i=0;i<n;i++){
+        cin>>x>>y;
+        v[x].push_back(y);
     }
-
-    sort(people.begin(), people.end(), comparer);
-
-    ll ind = -1;
-    ll prev = -1;
-    for(ll i = 0; i < people.size(); i++)
-    {
-        pii cur = people[i];
-        if(cur.f != prev)
-        {
-            prev = cur.f;
-            ind++;
-            a.pb({});
-        }
-
-        a[ind].pb(cur.s);
-
-        if(a[ind].size() >= 2)
-        {
-            //cout << a[ind][a.size() - 1] << " " << a[ind][a.size() - 2] << " " << a.size() << endl;
-           a[ind][a[ind].size() - 1] += a[ind][a[ind].size() - 2];
+    for(i=1;i<=m;i++){
+        if(v[i].size()>0){
+            sort(v[i].rbegin(),v[i].rend());
+            for(long long j=1;j<v[i].size();j++){
+                v[i][j]=v[i][j]+v[i][j-1];
+            }
         }
     }
-
-    if(a.size() > 1)
-        sort(a.begin(), a.end(), comparer2);
-    //cout << "hey";
-
-    ll mxSum = 0;
-    for(int j = 1; j <= n; j++)
-    {
-        ll curSum = 0;
-        for(int i = 0; i < a.size(); i++)
-        {
-            if(a[i].size() >= j && a[i][j - 1] > 0)
-                curSum += a[i][j - 1];
-            if(a[i].size() < j)
-                break;
+    for(long long i=1;i<=m;i++){
+        for(long long j=0;j<v[i].size();j++){
+            if(v[i][j]>0)
+            a[j]+=v[i][j];
         }
-        mxSum = max(mxSum, curSum);
     }
-    cout << mxSum << endl;
-    return  0;
+    long long Answer=0;
+    for(long long i=0;i<=n;i++)
+        Answer=max(Answer,a[i]);
+    cout<<Answer;
+    return 0;
 }
-
---
+++ POJ/poj3420.cc
@@ -0,0 +1,85 @@
+/*************************************************************************
+    > File Name: poj3420.cc
+    > Author: sqwlly
+    > Mail: sqw.lucky@gmail.com
+    > Created Time: 2018年12月01日 星期六 11时06分27秒
+ ************************************************************************/
+
+#include<iostream>
+#include<cstring>
+typedef long long LL;
+using namespace std;
+const int NUM = 4,MAXN = 4;
+int mod, dp[4] = {1,1,5,11};
+struct Matrix//矩阵的类
+{
+    LL a[NUM][NUM];
+    Matrix() {}
+    Matrix(LL *t)
+    {
+        int k = 0;
+        for(int i = 0; i < NUM; ++i)
+            for(int j = 0; j < NUM; ++j)
+                a[i][j] = t[k++];
+    }
+    void init()           //将其初始化为单位矩阵
+    {
+        memset(a, 0, sizeof(a));
+        for (int i = 0; i < MAXN; i++)
+            a[i][i] = 1;
+    }
+};
+
+Matrix mul(Matrix A,Matrix B)  //(a*b)%mod  矩阵乘法
+{
+    Matrix ans;
+    for (int i = 0; i < MAXN; i++)
+        for (int j = 0; j < MAXN; j++) {
+            ans.a[i][j] = 0;
+            for (int k = 0; k < MAXN; k++)
+                ans.a[i][j] = (ans.a[i][j] + A.a[i][k] * B.a[k][j]) % mod;
+        }
+    return ans;
+}
+
+LL solve(int n)
+{
+    LL t[16] = {1,5,1,-1,1,0,0,0,0,1,0,0,0,0,1,0};
+    Matrix ans, A(t);
+    ans.init();
+    while(n > 0) {
+        if(n & 1) ans = mul(ans,A);
+        A = mul(A,A);
+        n >>= 1;
+    }
+    LL ret = 0;
+    for(int i = 0; i < 4; ++i)
+        ret = (ret + ans.a[0][3 - i] * dp[i]) % mod;
+    return ret;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.in","r",stdin);
+#endif
+    int n;
+    //  [1 5 1 -1] ^ (n - 3) [1 ] =  [dp[n]    ] dp[n - 1]
+    //  [1 0 0  0]			 [1 ]    [dp[n - 1]] dp[n - 2]
+    //  [0 1 0  0]			 [5 ]    [dp[n - 2]] dp[n - 3]
+    //  [0 0 1  0]			 [11]    [dp[n - 3]] dp[n - 4]
+    //  dp[n] = dp[n - 1] + 5 * dp[n - 2] + dp[n - 3] + dp[n - 4];
+    //  dp[n - 1] = dp[n - 1]
+    //  dp[n - 2] = dp[n - 2]
+    //  dp[n - 3] = dp[n - 3]
+    while(cin >> n >> mod && (mod + n)) {
+        if(n < 4) {
+            cout << dp[n] << endl;
+        }else{
+            cout << solve(n - 3) << endl;
+        }
+    }
+
+    return 0;
+}
+
