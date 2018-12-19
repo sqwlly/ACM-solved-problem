1968828770822c4eeafda0f191ce02b8b177766e
sqwlly <sqw.lucky@gmail.com>
2018-12-20 00:30:19 +0800

    update

--- Codeforces/Codeforces Round #527 (Div. 3)/C.cc
+++ Codeforces/Codeforces Round #527 (Div. 3)/C.cc
@@ -8,34 +8,28 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-struct node{
-	string str;
-	int f, id;
-}s[300];
-
-
-bool cmp(node a,node b)
-{
-	return a.str.size() < b.str.size();
-}
-
-bool cmp1(node a,node b)
-{
-	return a.id < b.id;
-}
 int n;
-int dfs(int d,string now,int f)
+string s[202];
+char ans[204];
+
+bool solve(string pre,string suf)
 {
-	if(d > 2 * n - 2) return 1;
-	if(f == 1 && s[d].str.substr(0,now.size()) == now) {
-		if(d == 2 * n - 2 || d == 2 * n - 3) return 1;
-		dfs(d + 1, now, 1);
-	}else if(f == 0 && s[d].str.substr(s[d].str.size() - now.size(), s[d].str.size()) == now) {
-		if(d == 2 * n - 2 || d == 2 * n - 3) return 1;
-		dfs(d + 1, now, 0);
-	}else{
-		return 0;
+	memset(ans, 0, sizeof ans);
+	for(int i = 1; i <= n; ++i) {
+		bool f = 1;
+		for(int j = 0; j < 2 * n - 2; ++j) {
+			if(i == s[j].size()) {
+				if(pre.substr(0,i) == s[j] && f) {
+					ans[j] = 'P';
+					f = 0;
+				}else if(suf.substr(n - i - 1, i) == s[j]){
+					ans[j] = 'S';
+				}
+				if(!ans[j]) return 0;
+			}
+		}
 	}
+	return 1;
 }
 
 int main()
@@ -44,40 +38,22 @@ int main()
     freopen("input.in","r",stdin);
 #endif
 	cin >> n;
-	string ss;
+	string suf,pre;
 	for(int i = 0; i < 2 * n - 2; ++i) {
-		cin >> ss;
-		s[i].str = ss;
-		s[i].id = i;
-	}
-	sort(s,s + 2 * n - 2, cmp);
-	for(int i = 0; i < 2 * n - 2; i += 2) {
-		int f = dfs(i,s[i].str,1);
-		if(f) {
-			s[i].f = 1;
-			s[i + 1].f = 0;
-		}else{
-			s[i].f = 0;
-			s[i + 1].f = 1;
-		}
-	}
-	sort(s,s + 2 * n - 2, cmp1);
-	for(int i = 0; i < 2 * n - 2; ++i) {
-		putchar(s[i].f ? 'P' : 'S');
-	}
-/*	for(int i = 0; i < 2 * n - 2; ++i) {
-		int l = s[i].str.length(), f = 0;
-		for(int j = i + 2; j < 2 * n - 2; ++j) {
-			if(s[j].str.substr(0,l) == s[i].str) {
-				putchar('P');
-				f = 1;
-				break;
+		cin >> s[i];
+		if(s[i].size() == n - 1) {
+			if(!pre.size()) {
+				pre = s[i];
+			}else{
+				suf = s[i];
 			}
 		}
-		if(f == 0) {
-			putchar('S');
-		}
-	}*/
-	cout<<endl;
+	}
+	if(solve(suf,pre)) {
+		puts(ans);
+	}else if(solve(pre,suf)){
+		puts(ans);
+	}
+
     return 0;
 }
