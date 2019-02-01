2023fb7feac7307946e3b676bf46cef4ad8eb268
sqwlly <sqw.lucky@gmail.com>
2019-02-01 20:01:23 +0800

    update cf div2 536

--- Codeforces/Codeforces Round #536 (Div. 2)/D.cc
+++ Codeforces/Codeforces Round #536 (Div. 2)/D.cc
@@ -4,7 +4,6 @@
     > Mail: sqw.lucky@gmail.com 
     > Created Time: 2019年01月31日 星期四 21时43分53秒
  ************************************************************************/
-
 #include<bits/stdc++.h>
 #define P pair<int,int>
 using namespace std;
@@ -16,21 +15,21 @@ vector<int> ans;
 priority_queue<int,vector<int>,greater<int> > pq;
 void bfs()
 {
-	pq.push(1);
-	vis[1] = 1;
-	ans.push_back(1);
-	while(!pq.empty()) {
-		int cur = pq.top();
-		pq.pop();
-		for(int i = 0; !e[cur].empty(); e[cur].pop()) {
-			int v = e[cur].top();
-			if(vis[v] == 0) {
-				vis[v] = 1;
-				ans.push_back(v);
-			}
-			pq.push(v);
-		}
-	}
+    pq.push(1);
+    vis[1] = 1;
+    ans.push_back(1);
+    while(!pq.empty()) {
+        int cur = pq.top();
+        if(vis[cur] == 0) {
+            vis[cur] = 1;
+            ans.push_back(cur);
+        }
+        pq.pop();
+        for(; !e[cur].empty(); e[cur].pop()) {
+            int v = e[cur].top();
+            pq.push(v);
+        }
+    }
 }
 
 int main()
@@ -38,17 +37,18 @@ int main()
 #ifndef ONLINE_JUDGE
     freopen("input.in","r",stdin);
 #endif
-	int n,m,x,y;
-	cin >> n >> m;
-	for(int i = 0; i < m; ++i) {
-		scanf("%d%d",&x,&y);
-		e[x].push(y);
-		e[y].push(x);
-	}
-	bfs();
-	int len = ans.size();
-	for(int i = 0; i < len; ++i) {
-		printf("%d%c",ans[i],i==len-1?'\n':' ');
-	}
+    int n,m,x,y;
+    cin >> n >> m;
+    for(int i = 0; i < m; ++i) {
+        scanf("%d%d",&x,&y);
+        e[x].push(y);
+        e[y].push(x);
+    }
+    bfs();
+    int len = ans.size();
+    for(int i = 0; i < len; ++i) {
+        printf("%d%c",ans[i],i==len-1?'\n':' ');
+    }
     return 0;
 }
+
