265fa8a55b8c9d05f3fa39dcbe0035e7b65b4dea
sqwlly <sqw.lucky@gmail.com>
2019-02-06 20:33:14 +0800

    wuwuwu

--- wannafly_winter_camp_2018/day5/D.cc
+++ wannafly_winter_camp_2018/day5/D.cc
@@ -1,7 +1,7 @@
 /*************************************************************************
     > File Name: D.cc
     > Author: sqwlly
-    > Mail: sqw.lucky@gmail.com 
+    > Mail: sqw.lucky@gmail.com
     > Created Time: 2019年02月05日 星期二 14时32分09秒
  ************************************************************************/
 
@@ -9,51 +9,119 @@
 
 using namespace std;
 struct node{
-	int a,b;
+    int a,b;
 };
-vector<node> v[N];
-int c[10], r[10], n;
-int d[10][10];
-vector<int> g[20][7];
-void dfs(int row)
+int c[10], r[10], d[10][10], n;
+vector<int> g[20][20];
+vector<node> X[10];
+bool C[10][10], R[10][10];
+
+bool check()
 {
-	if(row == n + 1) return;
-	for(int k = 0; k < v[row].size(); ++k) {
-		memset(sum[row],0,sizeof sum[row]);
-		int a = v[row][k].a, b = v[row][k].b;
-		for(int i = a + 1; i < b; ++i) {
-			bool vis[10] = {0};
-			for(int num = 1; num <= r[row] - d[row][i - 1]; ++num) {
-				if(vis[num]) continue;
-				vis[num] = 1；
-				d[row][i] = num;
-				sum[row][i] = sum[row][i - 1] + d[row][i];
-				break;
-			}
-			if(sum[row][b] - sum[row][a] == r[row])
-			dfs(row + 1);
-		}
-	}
+    for(int i = 0; i < n; ++i) {
+        int sum = 0, j = 0;
+        while(d[j][i]) j++;
+
+        for(j = j + 1; d[j][i]; ++j) {
+            sum += d[j][i];
+        }
+        if(sum != c[i]) return 0;
+    }
+    return 1;
 }
-bool vis[10];
-void init(int sum,int k,int S)
+
+void show()
 {
-	int tmp = 0;
-	for(int i = 1; i <= k; ++i) {
-		if(S & (1 << i)) {
-			tmp += i;
-		}
-	}
-	if(tmp == sum) {
-		g[sum][k].push_back(S);
-		return;
-	}else if(tmp > sum) return;
-	for(int i = 1; i <= k; ++i) {
-		if(vis[i]) continue;
-		vis[i] = 1;
-		S |= (1 << i);
-		init(sum, k, S);
-	}
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (!d[i][j]) putchar('X');
+            else cout << d[i][j];
+        }
+        cout << endl;
+    }
+    cout<<endl;
+}
+
+void dfs(int row,int col) {
+    if (col == n) {
+        dfs(row + 1, 0);
+        return;
+    }
+    if (row == n) {
+        if (check()) {
+            show();
+            return;
+        }
+        return;
+    }
+    int a = X[row][0].a, b = X[row][0].b;
+    if (col > a && col < b) {
+        for (auto &S : g[r[row]][b - a - 1]) {
+            for (int i = 0; i < n - 2; ++i) {
+                if ((S & (1 << i)) && C[col][i] == 0 && R[row][i] == 0) {
+                    C[col][i] = R[row][i] = 1;
+                    d[row][col] = i + 1;
+                    dfs(row, col + 1);
+                    d[row][col] = 0;
+                    C[col][i] = R[row][i] = 0;
+                }
+            }
+        }
+    } else {
+        if (col == a || col == b) {
+            dfs(row, col + 1);
+        }
+        for (auto &S : g[r[row]][b - a - 1]) {
+            for (int i = 0; i < n - 2; ++i) {
+                if ((S & (1 << i)) == 0 && C[col][i] == 0 && R[row][i] == 0) {
+                    C[col][i] = R[row][i] = 1;
+                    d[row][col] = i + 1;
+                    dfs(row, col + 1);
+                    d[row][col] = 0;
+                    C[col][i] = R[row][i] = 0;
+                }
+            }
+        }
+    }
+}
+
+void dfs_(int row)
+{
+    if(row == n) {
+        memset(R,0,sizeof R);
+        memset(C,0,sizeof C);
+        dfs(0,0);
+        return;
+    }
+    for(int i = 0; i < n; ++i) {
+        for(int j = i + 1; j < n; ++j) {
+            if(g[r[row]][j - i - 1].size()) {
+                X[row].push_back(node{i,j});
+                dfs_(row + 1);
+                X[row].pop_back();
+            }
+        }
+    }
+}
+
+void init()
+{
+    int t = 0;
+    for(int i = 0; i < n - 2; ++i) {
+        t |= 1 << i;
+    }
+    g[0][0].push_back(0);
+    g[15][5].push_back(t);
+    for(int s = (t - 1) & t; s != 0; s = (s - 1) & t) {
+        int sum = 0, cnt = 0;
+        for(int i = 0; i < n - 2; ++i) {
+            if(1 & (s >> i)) {
+                sum += i + 1;
+                cnt++;
+            }
+        }
+        g[sum][cnt].push_back(s);
+    }
 }
 
 int main()
@@ -61,29 +129,22 @@ int main()
 #ifndef ONLINE_JUDGE
     freopen("input.in","r",stdin);
 #endif
-	int T;
-	cin >> T;
-	while(T--) {
-		cin >> n;
-		for(int i = 0; i < 2; ++i) {
-			for(int j = 0; j < n; ++j) {
-				if(i) cin >> r[i];
-				else cin >> c[i];
-			}
-		}
-		for(int row = 0; row < n; ++row) {
-			for(int i = 0; i < n; ++i) {
-				for(int j = i + 1; j < n; ++j) {
-					v[row].push_back(node{i,j});
-				}
-			}
-		}
-		for(int i = 1; i <= 15; ++i) {
-			for(int j = 1; j <= i; ++j) {
-				init(i,j,0);
-			}
-		}
-		dfs(0);
-	}
+    int T;
+    cin >> T;
+    while(T--) {
+        cin >> n;
+        for (auto &i : g)
+            i->clear();
+        init();
+        memset(d, 0, sizeof d);
+        for(int i = 0; i < 2; ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(i) cin >> c[j];
+                else cin >> r[j];
+            }
+        }
+        dfs_(0);
+    }
     return 0;
 }
+
