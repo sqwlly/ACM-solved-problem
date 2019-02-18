6ab1d0c36fcfb18f0e860b42c3b6bcd4fca07ce9
sqwlly <sqw.lucky@gmail.com>
2019-02-18 23:19:18 +0800

    add new cf edu 60

---
+++ Codeforces/Educational Codeforces Round 60 (Rated for Div. 2)/A.cc
@@ -0,0 +1,22 @@
+/*************************************************************************
+    > File Name: A.cc
+    > Author: sqwlly
+    > Mail: sqw.lucky@gmail.com 
+    > Created Time: 2019年02月18日 星期一 23时18分39秒
+ ************************************************************************/
+
+#include<bits/stdc++.h>
+
+#define DEBUG(x) std::cerr << #x << '=' << x << std::endl
+
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.in","r",stdin);
+#endif
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    return 0;
+}
