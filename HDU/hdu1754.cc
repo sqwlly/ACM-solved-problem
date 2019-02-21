d2fb7790200ba23dab6f22f0d860736a6d748e0a
sqwlly <sqw.lucky@gmail.com>
2019-02-22 00:36:12 +0800

    add a file hdu1754.cc

---
+++ HDU/hdu1754.cc
@@ -0,0 +1,22 @@
+/*************************************************************************
+    > File Name: hdu1754.cc
+    > Author: sqwlly
+    > Mail: sqw.lucky@gmail.com 
+    > Created Time: 2019年02月22日 星期五 00时35分46秒
+ ************************************************************************/
+
+#include<bits/stdc++.h>
+
+#define DEBUG(x) std::cerr << #x << '=' << x << std::endl
+
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.in","r",stdin);
+#endif
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    return 0;
+}
