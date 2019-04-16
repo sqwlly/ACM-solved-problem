ec0ed3259cd6bce4e0032939d5ea55ee039eb159
sqwlly <sqw.lucky@gmail.com>
2019-04-16 21:23:52 +0800

    update

---
+++ Codeforces/Codeforces Round #267 (Div. 2)/D.cc
@@ -0,0 +1,22 @@
+/*************************************************************************
+    > File Name: D.cc
+    > Author: sqwlly
+    > Mail: sqw.lucky@gmail.com 
+    > Created Time: 2019年04月16日 星期二 21时23分33秒
+ ************************************************************************/
+
+#include<bits/stdc++.h>
+
+#define DEBUG(x) std::cerr << #x << '=' << x << std::endl
+
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.in","r",stdin);
+#endif
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    return 0;
+}
