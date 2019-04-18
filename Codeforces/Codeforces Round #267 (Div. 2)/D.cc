6fe7f9794d7c71f86167b0f15ac66ef8dcae693d
sqwlly <sqw.lucky@gmail.com>
2019-04-18 13:55:15 +0800

    update

--- Codeforces/Codeforces Round #267 (Div. 2)/D.cc
+++ Codeforces/Codeforces Round #267 (Div. 2)/D.cc
@@ -9,11 +9,14 @@
 
 #define DEBUG(x) std::cerr << #x << '=' << x << std::endl
 #define P pair<string,vector<string>>
+typedef long long LL;
 using namespace std;
 const int N = 1E5+10;
 string s[N];
 map<string,vector<string>> E;
-map<string,int> cnt;
+map<string,LL> cnt;
+map<string,LL> len;
+map<string,bool> vis;
 vector<P> e;
 string to_lower(string & s)
 {
@@ -23,12 +26,21 @@ string to_lower(string & s)
 		if(c == 'r') ret++;
 	}
 	cnt[s] = ret;
+	len[s] = s.size();
 	return s;
 }
 
 void dfs(string u)
 {
+	vis[u] = 1;
 	for(const auto & v : E[u]) {
+		if(vis[v]) continue;
+		if(cnt[v] > cnt[u]) {
+			len[v] = len[u];
+			cnt[v] = cnt[u];
+		}else if(cnt[u] == cnt[v] && len[v] > len[u]) {
+			len[v] = len[u];
+		}
 		dfs(v);
 	}
 }
@@ -43,6 +55,7 @@ int main()
 	cin >> n;
 	for(int i = 0; i < n; ++i) {
 		cin >> s[i];
+		s[i] = to_lower(s[i]);
 	}
 	cin >> m;
 	string u,v;
@@ -52,11 +65,23 @@ int main()
 	}
 	for(const auto &i : E) {
 		e.emplace_back(i);
+		vis[i.first] = 0;
 	}
 	sort(e.begin(),e.end(),[](const P &x,const P &y) ->int{
 		if(cnt[x.first] == cnt[y.first]) return x.first.size() < y.first.size();
 		return cnt[x.first] < cnt[y.first];
 	});
-	for(auto i : e) cout << i.first << endl;
+	pair<LL,LL> ret = make_pair(0,0);
+	for(auto i : e) {
+		if(vis[i.first] == 0) {
+			vis[i.first] = 1;
+			dfs(i.first);
+		}
+	}
+	for(auto i : s) {
+		ret.first += cnt[i];
+		ret.second += len[i];
+	}
+	cout << ret.first << ' ' << ret.second << endl;
     return 0;
 }
