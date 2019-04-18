6f8b5b8c28a213e9f3f3ed7be1f47aff810da3a3
sqwlly <sqw.lucky@gmail.com>
2019-04-18 08:02:11 +0800

    update cf

--- Codeforces/Codeforces Round #552 (Div. 3)/C.cc
+++ Codeforces/Codeforces Round #552 (Div. 3)/C.cc
@@ -19,41 +19,24 @@ int main()
     ios::sync_with_stdio(false); cin.tie(0);
     int a[4] = {0}, b[4] = {0}, c[4] = {0};
     cin >> b[1] >> b[2] >> b[3];
-	for(int i = 1; i <= 3; ++i) a[i] = b[i];
-    int ret = 0, ans = 0;
-	pair<int,int> p;
+	int serv = min(b[1] / 3,min(b[2] / 2, b[3] / 2));
+	b[1] -= 3 * serv; b[2] -= 2 * serv; b[3] -= 2 * serv;
+    int ans = serv * 7;
+//	DEBUG(b[1]); DEBUG(b[2]); DEBUG(b[3]);
+	pair<int,int> p = make_pair(0,-1);
 	for(int s = 0; s < 7; ++s) {
 		int cnt = 0;
+		for(int i = 1; i <= 3; ++i) a[i] = b[i];
 		for(int i = s; ; i = (i + 1) % 7) {
 			if(a[food[i]] <= 0) break;
 			a[food[i]]--;
 			cnt++;
-			if(cnt >= 7) break;
 		}
 		if(cnt > p.second) {
 			p = make_pair(s,cnt);
 		}
 	}
-
-    for(int s = 0; s < 7; ++s) {
-        ret = 0;
-		for(int i = 1; i <= 3; ++i) a[i] = b[i];
-		int cur = s;
-		c[1] = c[2] = c[3] = 0;
-		for(int i = 0; i < 7; ++i) {
-			c[food[cur]]++;
-		}
-		
-        for(int i = s; 1; i = (i + 1) % 7) {
-          //  DEBUG(i);
-            assert(food[i] >= 1);
-            if(a[food[i]] <= 0) break;
-            a[food[i]]--;
-            ret++;
-        }
-        ans = max(ret, ans);
-    }
-    cout << ans << endl;
+    cout << ans + p.second << endl;
     return 0;
 }
 
